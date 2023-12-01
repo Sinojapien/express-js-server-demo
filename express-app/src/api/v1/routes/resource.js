17f07dd9dce31cc57271919f9e433f08a2ac8d88
Sinojapien <1155126460@link.cuhk.edu.hk>
2023-12-01 17:10:13 -0500

    REST api: follow best practice

--- express-app/src/api/v1/routes/resource.js
+++ express-app/src/api/v1/routes/resource.js
@@ -1,26 +1,18 @@
 const router = require("express").Router();
 const { get, create, update, remove } = require("../controllers/resource");
 
-const BASE_ROUTE = "/resource";
+const BASE_ROUTE = "/resources";
 
 // Reading resource
 router.get(`${BASE_ROUTE}/:id`, require("../middlewares/cache"), get);
 
 // Creating resource
-router.post(`${BASE_ROUTE}/create`, require("../middlewares/no-cache"), create);
+router.post(`${BASE_ROUTE}`, require("../middlewares/no-cache"), create);
 
 // Updating resource
-router.patch(
-  `${BASE_ROUTE}/:id/update`,
-  require("../middlewares/no-cache"),
-  update
-);
+router.patch(`${BASE_ROUTE}/:id`, require("../middlewares/no-cache"), update);
 
 // Deleting resource
-router.delete(
-  `${BASE_ROUTE}/:id/delete`,
-  require("../middlewares/no-cache"),
-  remove
-);
+router.delete(`${BASE_ROUTE}/:id`, require("../middlewares/no-cache"), remove);
 
 module.exports = router;
